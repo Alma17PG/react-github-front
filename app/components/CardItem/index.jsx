e46371c4971542494edc0e273dd10f4553185809
Alma <alma17pg@gmail.com>
2021-03-25 18:53:17 -0600

    refactor: adding onClick in CardItem component

--- app/components/CardItem/index.jsx
+++ app/components/CardItem/index.jsx
@@ -7,8 +7,17 @@ import CardDescription from './CardDescription';
 
 import './style.sass';
 
-const CardItem = ({ image, title, description }) => (
-  <div className="card card-item">
+const CardItem = ({
+  image, title, description,
+  onClick,
+}) => (
+  <div
+    className="card card-item"
+    role="button"
+    tabIndex="0"
+    onKeyPress={() => { }}
+    onClick={() => onClick(title)}
+  >
     <CardImage image={image} />
     <div className="card-content">
       <CardTitle title={title} />
@@ -21,12 +30,14 @@ CardItem.propTypes = {
   image: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 CardItem.defaultProps = {
   image: '',
   title: '',
   description: '',
+  onClick: () => { },
 };
 
 export default memo(CardItem);
